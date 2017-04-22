350248d3abd0021b8348dca3781505622012b2f5
Wil Taylor <wilfridtaylor@gmail.com>
2017-04-22 16:10:51 +1000

    Added camera controls

---
+++ LudumDare/LD38/Assets/Scripts/UserInput/UserInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public float ScrollSpeed;
+
+    private void Start()
+    {
+        
+    }
+
+	private void Update ()
+    {
+        if (Input.GetAxis("Vertical") > 0.1f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime * ScrollSpeed, transform.position.z);
+            Debug.Log("Pressed Up");
+        }
+
+        if (Input.GetAxis("Vertical") < -0.1f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - Time.deltaTime * ScrollSpeed, transform.position.z);
+            Debug.Log("Pressed Down");
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.position = new Vector3(transform.position.x + Time.deltaTime * ScrollSpeed, transform.position.y, transform.position.z);
+            
+            Debug.Log("Pressed Right");
+        }
+
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.position = new Vector3(transform.position.x - Time.deltaTime * ScrollSpeed, transform.position.y, transform.position.z);
+            Debug.Log("Pressed Left");
+        }
+
+
+
+    }
+}
