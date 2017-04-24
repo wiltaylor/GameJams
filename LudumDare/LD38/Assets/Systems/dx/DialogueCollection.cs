c9fc3e21783578278a6ac0cd8f50106f1f5ca3cf
Wil Taylor <wilfridtaylor@gmail.com>
2017-04-24 16:15:58 +1000

    Stored Dialougue in unity

---
+++ LudumDare/LD38/Assets/Systems/dx/DialogueCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Systems.dx
+{
+    public enum DialogueCharecter
+    {
+        None,
+        Knight,
+        Cardinal,
+        Deamon
+    }
+
+    [CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue/Dialogue Entry", order = 1)]
+    public class DialogueCollection : ScriptableObject
+    {
+        public string Name;
+        public DialogueEntry[] Dialogue;
+    }
+
+    [Serializable]
+    public class DialogueEntry
+    {
+        public DialogueCharecter Charecter;
+        public string Text;
+    }
+}
---
+++ LudumDare/LD38/Assets/Systems/dx/DialogueService.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+namespace Assets.Systems.dx
+{
+    public class DialogueService
+    {
+        private static DialogueService _instance;
+        private DialogueCollection[] _dialogueCollections;
+        private DialogueCollection _currentDialogue;
+        private int _currentDialogueIndex;
+
+        public DialogueService Instance
+        {
+            get { return _instance ?? (_instance = new DialogueService()); }
+        }
+
+        public void AssignDialogue(DialogueCollection[] dialogue)
+        {
+            _dialogueCollections = dialogue;
+        }
+
+        public void StartDialoue(string name)
+        {
+            _currentDialogue = _dialogueCollections.FirstOrDefault(d => d.Name == name);
+            _currentDialogueIndex = 0;
+        }
+
+        public bool MoreDialogue()
+        {
+            if (_currentDialogue != null && _currentDialogueIndex >= _currentDialogue.Dialogue.Length)
+                _currentDialogue = null;
+
+            return _currentDialogue != null;
+        }
+
+        public DialogueCharecter CurrentCharecter
+        {
+            get { return _currentDialogue.Dialogue[_currentDialogueIndex].Charecter; }
+        }
+
+        public string CurrentText
+        {
+            get { return _currentDialogue.Dialogue[_currentDialogueIndex].Text; }
+        }
+    }
+}
