f69038b86cbdc8e8cad68598eca4b2dea6e8043e
Wil Taylor <wilfridtaylor@gmail.com>
2018-01-27 00:11:50 +1100

    Added rotation script

---
+++ GlobalGameJam/GGJ2018/Assets/Rotator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    public enum RotationDirection
+    {
+        Left,
+        Right,
+        Forward,
+        Back
+    }
+
+    public float Speed;
+    public bool Always;
+
+    private Vector3 _targetRotation;
+
+    // Update is called once per frame
+	void Update ()
+    {
+        if (Always)
+        {
+            transform.Rotate(0, Speed * Time.deltaTime, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.eulerAngles, _targetRotation, Time.deltaTime * Speed));
+        }
+
+        
+	}
+
+    public void Rotate(float angle)
+    {
+        _targetRotation = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + angle, transform.rotation.eulerAngles.z);
+    }
+}
