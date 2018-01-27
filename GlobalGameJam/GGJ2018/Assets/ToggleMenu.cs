8ed151684448a2a4555037f7d3003d3f377270ae
Wil Taylor <wilfridtaylor@gmail.com>
2018-01-27 21:57:48 +1100

    Added in game menu

---
+++ GlobalGameJam/GGJ2018/Assets/ToggleMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleMenu : MonoBehaviour
+{
+    public GameObject MenuObject;
+    public float TimeOut = 0.5f;
+
+    private float _currentTimeout = 0f;
+
+	void Update ()
+	{
+	    if (_currentTimeout >= 0f)
+	        _currentTimeout -= Time.deltaTime;
+
+
+
+	    if (Input.GetButton("Cancel") && _currentTimeout <= 0f)
+	    {
+	        MenuObject.SetActive(!MenuObject.activeInHierarchy);
+	        _currentTimeout = TimeOut;
+
+	    }
+	}
+}
