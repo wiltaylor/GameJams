5c60153be7c0f19e57037b89c7097ce0402bfe6c
Unknown <geoff@crystalclearit.com.au>
2018-01-28 10:50:52 +1100

    Portal

---
+++ GlobalGameJam/GGJ2018/Assets/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    // The index of the scene to load. Check that the scene that you want to load is in the Build Settings.
+    [Tooltip("The index of the scene to load. Check that the scene that you want to load is in the Build Settings.")]
+    public int SceneIndexToLoad = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Load the next Scene.
+        if (other.gameObject.tag == "Player") SceneManager.LoadScene(SceneIndexToLoad);
+    }
+}
