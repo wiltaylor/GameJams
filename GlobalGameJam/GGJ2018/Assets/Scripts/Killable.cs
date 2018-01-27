327910163e3f20e2963a93c65e136d2e9ba7a94f
Wil Taylor <wilfridtaylor@gmail.com>
2018-01-27 20:45:53 +1100

    Spawn point

--- GlobalGameJam/GGJ2018/Assets/Scripts/Killable.cs
+++ GlobalGameJam/GGJ2018/Assets/Scripts/Killable.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Killable : MonoBehaviour
 {
     public float KillOnY = -100f;
+    private List<SpawnController> _spawns = new List<SpawnController>();
+
+    void Start()
+    {
+        if (tag != "Player")
+            return;
+
+        _spawns.AddRange(FindObjectsOfType<SpawnController>());
+    }
 
     public void Kill()
     {
-        Destroy(gameObject);
+        if (tag != "Player")
+            Destroy(gameObject);
+        else
+        {
+            var spawn = _spawns.FirstOrDefault(s => s.ActiveSpawn) ?? _spawns.FirstOrDefault();
+
+            if(spawn == null)
+                Debug.LogError("NO RESPAWN POINTS IN LEVEL. CAN'T RESPAWN PLAYER");
+
+            transform.position = spawn.transform.position;
+            transform.parent = null;
+        }
     }
 
     void Update()
---
+++ GlobalGameJam/GGJ2018/Assets/SpawnController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+
+    private List<SpawnController> _spawns = new List<SpawnController>();
+
+    public bool ActiveSpawn;
+
+	void Start ()
+	{
+	    _spawns.AddRange(FindObjectsOfType<SpawnController>());
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ActiveSpawn = true;
+
+            foreach(var spawn in _spawns)
+                if (spawn != this)
+                    spawn.ActiveSpawn = false;
+        }
+    }
+}
